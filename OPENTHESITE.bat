d9726339fefb52cdfb55efc06d96cdb561434e12
NeezyCodes <52862870+NeezyCodes@users.noreply.github.com>
2019-10-16 20:24:20 +0200

    adding main folder files

---
+++ OPENTHESITE.bat
@@ -0,0 +1,29 @@
+@echo off
+color fc
+echo IMPORTANT:
+echo IMPORTANT:
+echo IMPORTANT:
+echo IMPORTANT:
+echo IMPORTANT:
+echo IMPORTANT:
+echo IMPORTANT:
+echo.
+echo DONT OPEN WITH KOMPOZER!
+echo THIS PROGRAM KILLS "unused" TAGS! But they are important!!!
+pause
+echo Site has loading Issues due to MANY Scripts (js files and Folders)
+echo Please Wait AFTER Loading Screen around 10-20 seconds
+pause
+pause
+pause
+echo only two buttons left ;-)
+pause
+pause
+dontopenme.txt
+pause
+@ping -n 2 localhost> nul
+echo.
+echo Starting Site.. Please wait.
+@ping -n 4 localhost> nul
+echo started.
+index.html
